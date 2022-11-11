b1af205a25b1a72464b165d9c5d55595868c15d8
ssthapi <ssthapi@ncsu.edu>
2022-11-11 18:04:37 -0500

    revision 3

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 $(document).ready(function() {
-    let currentPage = 0;
+
+    const url = window.location.href.split('?')[0];
+    const queryString = window.location.search;
+    let urlParams = new URLSearchParams(queryString);
+    const urlPageNum = urlParams.get('page')
     let totalPageNum = $(".page-wrapper").length
+    let currentPage = 0
+
+    if (urlPageNum) {
+        if (urlPageNum >= 0 && urlPageNum < totalPageNum) {
+            currentPage = parseInt(urlPageNum);
+        }
+    }
+
+    let pageCnt = 1
+    $(".page-wrapper").each(function() {
+        $(this).attr("id", "page-" + pageCnt);
+        pageCnt += 1;
+    })
+
 
     initPages()
 
-    let labels = ["Introduction", "HTML", "HTML TAGS", "MORE TAGS", "ATTRIBUTES", "CSS", "Selectors", "Advanced CSS", "PRACTICE", "END"]
-    for (i = 1; i < 11; i++) {
-        let navElement = `<div class="nav-button-container" id="nav-page-` + i + `" >
-                            <div class="nav-button">
-                                <p>` + i + `</p>
-                            </div>
-                            <p class="nav-label">` + labels[i - 1] + `</p>
-                        </div>`
-        $("#nav-bar").append(navElement);
-    }
+    // let labels = ["Introduction", "HTML STRUCTURE", "MORE TAGS", "ATTRIBUTES", "CSS", "Selectors", "Advanced CSS", "PRACTICE", "END"]
+    // for (i = 1; i < 10; i++) {
+    //     let navElement = `<div class="nav-button-container" id="nav-page-` + i + `" >
+    //                         <div class="nav-button">
+    //                             <p>` + i + `</p>
+    //                         </div>
+    //                         <p class="nav-label">` + labels[i - 1] + `</p>
+    //                     </div>`
+    //     $("#nav-bar").append(navElement);
+    // }
+
     $(".nav-button-container").on("click", function(e) {
-            let pageIdx = parseInt(e.target.innerText)
-            currentPage = pageIdx
-            movePage(currentPage)
-        })
-        // movePage(currentPage);
+        let pageIdx = parseInt(e.target.innerText)
+        currentPage = pageIdx
+        movePage(currentPage)
+    })
+
+    $('#css-importance').on("click", function() {
+        $('link[rel="stylesheet"], style').remove();
+        $('*').removeAttr('style');
+    })
+    $("#hidden-go-back").on("click", function() {
+        window.location.href = url + "?page=1";
+    })
+
+    movePage(currentPage);
 
     function initPages() {
         for (i = 0; i < totalPageNum; i++) {
@@ -36,8 +64,18 @@ $(document).ready(function() {
     }
 
     $('.next-button').on("click", function() {
+        // console.log(currentPage)
         currentPage += 1;
+        // console.log(currentPage)
         if (currentPage < totalPageNum) {
+            movePage(currentPage)
+        }
+    })
+    $('.back-button').on("click", function() {
+        // console.log(currentPage)
+        currentPage -= 1;
+        // console.log(currentPage)
+        if (currentPage >= 0) {
             movePage(currentPage)
         }
     })
