06ac32c20914212871a3048188f3c1d79b116a60
ssthapi <ssthapi@ncsu.edu>
2022-11-11 23:06:14 -0500

    usable-v1

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
     let totalPageNum = $(".page-wrapper").length
     let currentPage = 0
 
+
     if (urlPageNum) {
         if (urlPageNum >= 0 && urlPageNum < totalPageNum) {
             currentPage = parseInt(urlPageNum);
@@ -21,45 +22,72 @@ $(document).ready(function() {
 
 
     initPages()
+    movePage(currentPage);
 
-    // let labels = ["Introduction", "HTML STRUCTURE", "MORE TAGS", "ATTRIBUTES", "CSS", "Selectors", "Advanced CSS", "PRACTICE", "END"]
-    // for (i = 1; i < 10; i++) {
-    //     let navElement = `<div class="nav-button-container" id="nav-page-` + i + `" >
-    //                         <div class="nav-button">
-    //                             <p>` + i + `</p>
-    //                         </div>
-    //                         <p class="nav-label">` + labels[i - 1] + `</p>
-    //                     </div>`
-    //     $("#nav-bar").append(navElement);
-    // }
-
-    $(".nav-button-container").on("click", function(e) {
-        let pageIdx = parseInt(e.target.innerText)
-        currentPage = pageIdx
-        movePage(currentPage)
+    // nav cl;ick functions
+    $("#nav-1").on("click", function() {
+        if (currentPage > 2) {
+            currentPage = 0;
+            movePage(currentPage);
+        }
     })
 
+    $("#nav-2").on("click", function() {
+        if (currentPage > 5 || currentPage < 3) {
+            currentPage = 3;
+            movePage(currentPage);
+        }
+    })
+    $("#nav-3").on("click", function() {
+        if (currentPage > 8 || currentPage < 6) {
+            currentPage = 6;
+            movePage(currentPage);
+        }
+    })
+    $("#nav-4").on("click", function() {
+        if (currentPage < 9) {
+            currentPage = 9;
+            movePage(currentPage);
+        }
+    })
+
+
+    // display css importance
     $('#css-importance').on("click", function() {
         $('link[rel="stylesheet"], style').remove();
         $('*').removeAttr('style');
     })
     $("#hidden-go-back").on("click", function() {
-        window.location.href = url + "?page=1";
+        window.location.href = url + "?page=2";
     })
 
-    movePage(currentPage);
 
     function initPages() {
         for (i = 0; i < totalPageNum; i++) {
             let pageId = "page-" + (i + 1);
             $("#" + pageId).css("left", i * 100 + "%");
         }
+        colorNav()
     }
 
     function movePage(index) {
         for (i = 0; i < totalPageNum; i++) {
             let pageId = "page-" + (i + 1);
             $("#" + pageId).css("left", (i - index) * 100 + "%");
+        }
+        colorNav()
+    }
+
+    function colorNav() {
+        $(".nav-button").removeClass("active");
+        if (currentPage < 3) {
+            $("#nav-1").addClass("active");
+        } else if (currentPage < 6) {
+            $("#nav-2").addClass("active");
+        } else if (currentPage < 9) {
+            $("#nav-3").addClass("active");
+        } else {
+            $("#nav-4").addClass("active");
         }
     }
 
